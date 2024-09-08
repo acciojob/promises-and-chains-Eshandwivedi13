e6b12e6b540b51ba6adcff53315e9b0e3efdbe94
Eshan Dwivedi <79198616+Eshandwivedi13@users.noreply.github.com>
2024-09-08 15:44:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const ageInput = document.getElementById('age');
+const nameInput = document.getElementById('name');
+const buttonSubmit = document.getElementById('btn');
+
+// if(ageInput.value = ' ' || nameInput.value = ' ') 
+
+buttonSubmit.addEventListener('click', (e) => {
+	e.preventDefault();
+	if(ageInput.value === ' ' || nameInput.value === ' ') alert('inputs cannot be empty');
+	return new Promise((res, rej) => {
+		setTimeout(() => {
+			if(ageInput.value > 18) res(ageInput.value);
+			else rej(ageInput.value);
+		}, 4000);
+	}).then((data) => {
+		alert('Welcome, ' + data + '. You can vote.');
+	}).catch((data) => {
+		alert('Oh sorry ' + data + ". You aren't old enough.");
+	})
+})
+
